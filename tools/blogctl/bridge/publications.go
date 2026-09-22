b1029d6749f2c8202676705157191c7235c2e913
ThinkerQAQ <zhangshengkunz@gmail.com>
2026-09-22 15:14:37 +0800

    feat(blogctl): add publication inventory endpoint

---
+++ tools/blogctl/bridge/publications.go
@@ -0,0 +1,20 @@
+package bridge
+
+import (
+	"net/http"
+
+	"github.com/ThinkerQAQ/ThinkerQAQ.github.io/tools/blogctl/publisher"
+)
+
+func (s *Server) handlePublications(response http.ResponseWriter) {
+	s.mu.Lock()
+	contentRoot := s.config.ContentRoot
+	s.mu.Unlock()
+
+	records, err := publisher.ListPublicationRecords(contentRoot)
+	if err != nil {
+		writeAPIError(response, http.StatusBadRequest, "invalid_request", err.Error(), nil)
+		return
+	}
+	writeJSON(response, http.StatusOK, map[string]any{"records": records})
+}
