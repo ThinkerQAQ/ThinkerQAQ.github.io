1318002d934757e959cecf037f9c95a47038507f
ThinkerQAQ <zhangshengkunz@gmail.com>
2026-09-22 18:01:44 +0800

    refactor(blogctl): share platform capabilities across packages

--- tools/blogctl/publisher/capabilities.go
+++ tools/blogctl/publisher/capabilities.go
@@ -1,50 +1,9 @@
 package publisher
 
-type PlatformCapabilities struct {
-	BrowserSession  bool `json:"browserSession"`
-	APIKey          bool `json:"apiKey"`
-	DraftCreate     bool `json:"draftCreate"`
-	DraftUpdate     bool `json:"draftUpdate"`
-	ExplicitPublish bool `json:"explicitPublish"`
-	PublishedUpdate bool `json:"publishedUpdate"`
-	RemoteList      bool `json:"remoteList"`
-	BodyImages      bool `json:"bodyImages"`
-}
+import blogplatform "github.com/ThinkerQAQ/ThinkerQAQ.github.io/tools/blogctl/platform"
 
-var platformCapabilities = map[string]PlatformCapabilities{
-	"cnblogs": {
-		BrowserSession: true, DraftCreate: true, DraftUpdate: true, ExplicitPublish: true,
-		PublishedUpdate: true, RemoteList: true, BodyImages: true,
-	},
-	"juejin": {
-		BrowserSession: true, DraftCreate: true, DraftUpdate: true, ExplicitPublish: true, BodyImages: true,
-	},
-	"csdn": {
-		BrowserSession: true, DraftCreate: true, DraftUpdate: true, ExplicitPublish: true, BodyImages: true,
-	},
-	"segmentfault": {
-		BrowserSession: true, DraftCreate: true, DraftUpdate: true, ExplicitPublish: true, BodyImages: true,
-	},
-	"zhihu": {
-		BrowserSession: true, DraftCreate: true, DraftUpdate: true, ExplicitPublish: true, BodyImages: true,
-	},
-	"51cto": {
-		BrowserSession: true, DraftCreate: true, DraftUpdate: true, ExplicitPublish: true, BodyImages: true,
-	},
-	"oschina": {
-		BrowserSession: true, DraftCreate: true, DraftUpdate: true, ExplicitPublish: true, BodyImages: true,
-	},
-	"toutiao": {
-		BrowserSession: true, DraftCreate: true, DraftUpdate: true, ExplicitPublish: true, BodyImages: true,
-	},
-	"devto": {
-		APIKey: true, DraftCreate: true, DraftUpdate: true, RemoteList: true, BodyImages: true,
-	},
-	"medium": {
-		BrowserSession: true, DraftCreate: true,
-	},
-}
+type PlatformCapabilities = blogplatform.Capabilities
 
 func PlatformCapabilitiesFor(platform string) PlatformCapabilities {
-	return platformCapabilities[platform]
+	return blogplatform.For(platform)
 }
