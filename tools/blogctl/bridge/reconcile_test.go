d91ed8c567bf9ae8ee6384719dd58a7d14db8e47
ThinkerQAQ <zhangshengkunz@gmail.com>
2026-09-22 18:13:16 +0800

    style(blogctl): gofmt reconciliation tests

--- tools/blogctl/bridge/reconcile_test.go
+++ tools/blogctl/bridge/reconcile_test.go
@@ -65,9 +65,9 @@ func TestDEVToPublicationReconciliationUsesExactRemoteID(t *testing.T) {
 		body := `{"id":42,"title":"Example","url":"https://dev.to/user/example","published":false}`
 		return &http.Response{
 			StatusCode: http.StatusOK,
-			Body: io.NopCloser(bytes.NewBufferString(body)),
-			Header: make(http.Header),
-			Request: request,
+			Body:       io.NopCloser(bytes.NewBufferString(body)),
+			Header:     make(http.Header),
+			Request:    request,
 		}, nil
 	})}
 
