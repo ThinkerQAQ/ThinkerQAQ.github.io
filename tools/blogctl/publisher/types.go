67ead1589d7c93bfcd03e2ee6913ab60e60f2153
ThinkerQAQ <zhangshengkunz@gmail.com>
2026-09-23 18:18:23 +0800

    style(blogctl): apply gofmt alignment

--- tools/blogctl/publisher/types.go
+++ tools/blogctl/publisher/types.go
@@ -21,7 +21,7 @@ type Session struct {
 	Cookies             []BrowserCookie
 	UserAgent           string
 	RequestCookieHeader string
-	CookieHostSuffixes   []string
+	CookieHostSuffixes  []string
 	APIKey              string
 }
 
