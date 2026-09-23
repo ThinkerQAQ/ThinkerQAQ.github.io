e0d7f4fd142ce617b823a9b552e4779544cd9153
ThinkerQAQ <zhangshengkunz@gmail.com>
2026-09-23 12:44:59 +0800

    test(blogctl): cover Zhihu and OSChina list capabilities

--- tools/blogctl/publisher/capabilities_test.go
+++ tools/blogctl/publisher/capabilities_test.go
@@ -22,6 +22,15 @@ func TestPlatformCapabilitiesMatchCurrentControlPlane(t *testing.T) {
 		{"toutiao", func(value PlatformCapabilities) bool {
 			return value.BrowserSession && value.DraftCreate && value.DraftUpdate && value.ExplicitPublish && value.BodyImages && value.BodyImageRehost
 		}},
+		{"zhihu", func(value PlatformCapabilities) bool {
+			return value.BrowserSession && value.DraftCreate && value.DraftUpdate && value.ExplicitPublish &&
+				value.RemoteList && value.BodyImages && value.BodyImageRehost
+		}},
+		{"oschina", func(value PlatformCapabilities) bool {
+			return value.BrowserSession && value.DraftCreate && value.DraftUpdate && value.ExplicitPublish &&
+				value.RemoteList && value.BodyImages && value.BodyImageRehost
+		}},
+
 	}
 	for _, tc := range cases {
 		t.Run(tc.platform, func(t *testing.T) {
