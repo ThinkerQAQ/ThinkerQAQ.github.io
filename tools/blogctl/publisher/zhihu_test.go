2fd130480389c837263b3a445a4b7717d9f42f69
ThinkerQAQ <zhangshengkunz@gmail.com>
2026-09-24 11:50:18 +0800

    test(blogctl): provide Zhihu browser session for image upload

--- tools/blogctl/publisher/zhihu_test.go
+++ tools/blogctl/publisher/zhihu_test.go
@@ -152,7 +152,10 @@ func TestZhihuPrepareHTMLUploadsGeneratedAssetWithoutR2(t *testing.T) {
 		}
 	})}
 
-	adapterValue, err := NewZhihuAdapter(client, Session{UserAgent: "BlogCTL-Test-UA"})
+	adapterValue, err := NewZhihuAdapter(client, Session{
+		UserAgent: "BlogCTL-Test-UA",
+		Cookies: []BrowserCookie{{Name: "z_c0", Value: "test", Domain: ".zhihu.com", Path: "/", Secure: true}},
+	})
 	if err != nil {
 		t.Fatal(err)
 	}
